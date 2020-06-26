6c1e5756581cacfd996fa9e35f8087e23d8996a4
Becky Afzali <beckyafzali@gmail.com>
2020-06-26 14:28:24 -0500

    fbase automatic endturn issue resolved

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -36,12 +36,9 @@ import GameBoard from "@/components/GameBoard.vue"
 export default {
   data() {
     return {
-      // isSlotFilled: [false, false, false, false, false],
       rollResult: 0,
       isUsersTurn: null,
       currentGameID: this.$route.params.id,
-      // coinSlotArray: this.currentGame.isSlotFilled.split(",")
-      // opponent: this.opponentUserName()
     }
   },
   components: {
@@ -62,17 +59,8 @@ export default {
       
       return this.currentGame.ownerUserName
     },
-    checkOwnerScore() {
-      return this.currentGame.ownerScore
-    },
-    checkOpponentScore() {
-      return this.currentGame.opponentScore
-    },
     checkActivePlayer() {
       return this.currentGame.activePlayerID
-    },
-    checkIsSlotFilled() {
-      return this.currentGame.isSlotFilled
     },
     coinSlotArray: function() {
       return this.currentGame.isSlotFilled.split(",")
@@ -86,66 +74,38 @@ export default {
     }
   },
   watch: {
-    checkActivePlayer(newVal, oldVal) {
+    checkActivePlayer(newVal) {
       if(newVal === this.currentUser.uid) {
         this.isUsersTurn = true
       } else {
         this.isUsersTurn = false
       }
-
-      fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
-        activePlayerID: newVal 
-      }).then(() => {
-        console.log("Active player changed from " + oldVal + " to " + newVal);
-      }).catch((err) => {
-        console.log(err);
-      })
-    },
-    checkIsSlotFilled(newVal) {
-      console.log("Watcher: " + newVal);
-      
-      fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
-        isSlotFilled: newVal 
-      }).then(() => {
-        console.log("slots updated to firebase");
-      }).catch((err) => {
-        console.log(err);
-      })
-    },
-    checkOwnerScore(newVal) {
-      fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
-        ownerScore: newVal 
-      }).then(() => {
-        console.log("Owner's score updated");
-      }).catch((err) => {
-        console.log(err);
-      })
-    },
-    checkOpponentScore(newVal) {
-      fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
-        opponentScore: newVal 
-      })
-      .then(() => {
-        console.log("Opponent's score updated");
-      }).catch((err) => {
-        console.log(err);
-      })
     },
   },
   methods: {
-    rollDie() {
-      let rollResult = Math.floor((Math.random() * 6) + 1)
-      this.rollResult = rollResult
-      console.log(rollResult);
-      let rollIndex = rollResult - 1
-      this.updateScore(rollIndex)
+    async rollDie() {
+      try {
+        let rollResult = Math.floor((Math.random() * 6) + 1)
+        this.rollResult = rollResult
+        console.log(rollResult + " FIRST | roll result");
+        let rollIndex = rollResult - 1
+
+        const scoreChange = await this.processRollOutcome(rollIndex)
+
+        console.log(scoreChange + " THIRD | scoreChange set");
+
+        this.processScoreChange(scoreChange)
+        
+      } catch(error) {
+        console.log(error);
+      }
     },
-    updateScore(rollIndex) {
+    processRollOutcome(rollIndex) {
       let scoreChange = 0
       let coinSlotArray = this.currentGame.isSlotFilled.split(",")
 
       if(coinSlotArray[rollIndex] === "false" && rollIndex < 5) {
-
+        
         coinSlotArray[rollIndex] = "true"
         scoreChange--
         
@@ -159,34 +119,88 @@ export default {
 
         coinSlotArray=["false", "false", "false", "false", "false"]
         
-        // this.endTurn()
       } else {
         scoreChange--
       }
+      console.log("SECOND | scoreChange logic complete");
 
-      this.$store.commit("setIsSlotFilled", coinSlotArray.toString())
+      fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+        isSlotFilled: coinSlotArray.toString() 
+      }).then(() => {
+        console.log("slots updated to firebase");
+      }).catch((err) => {
+        console.log(err);
+      })
+      
+      // this.$store.commit("setIsSlotFilled", coinSlotArray.toString())
+
+      return scoreChange
+    },
+    processScoreChange(scoreChange) {
       
       if (this.currentGame.ownerID === this.currentUser.uid) {
-        this.$store.commit("incrementOwnerScore", scoreChange)
-      } 
-      else {
-        this.$store.commit("incrementOpponentScore", scoreChange)
+        console.log("FOURTH | increment owner score");
+        fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+          ownerScore: this.currentGame.ownerScore + scoreChange 
+        }).then(() => {
+          console.log("Owner's score updated");
+          // if user rolls a number already filled, end their turn
+          if (scoreChange > 1 || this.currentGame.isSlotFilled === "false,false,false,false,false" 
+              && this.rollResult !== 6) {
+              this.endTurn()
+          }
+        }).catch((err) => {
+          console.log(err);
+        })
+      } else {
+        console.log("FOURTH | increment owner score");
+        fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+          opponentScore: this.currentGame.opponentScore + scoreChange 
+        }).then(() => {
+          console.log("Opponent's score updated");
+        // if user rolls a number already filled, end their turn
+          if (scoreChange > 1 || this.currentGame.isSlotFilled === "false,false,false,false,false"
+              && this.rollResult !== 6) {
+              this.endTurn()
+          }
+        }).catch((err) => {
+          console.log(err);
+        })
       } 
     },
     endTurn() {
       if (this.currentUser.uid === this.currentGame.ownerID) {
-        this.$store.commit("setActivePlayerID", this.currentGame.opponentID)
+        console.log("FIFTH | End turn, opponentID set to active");
+
+        fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+          activePlayerID: this.currentGame.opponentID 
+        }).then(() => {
+          console.log("Active player changed from " + this.currentGame.ownerID 
+          + " to " + this.currentGame.opponentID);
+        }).catch((err) => {
+          console.log(err);
+        })
       } else {
-        this.$store.commit("setActivePlayerID", this.currentGame.ownerID)
+        console.log("FIFTH | End turn, ownerID set to active");
+
+        fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+          activePlayerID: this.currentGame.ownerID 
+        }).then(() => {
+          console.log("Active player changed from " + this.currentGame.opponentID 
+          + " to " + this.currentGame.ownerID);
+        }).catch((err) => {
+          console.log(err);
+        })
       }
     },
     // updateGameFieldInFirestore(field, value) {
-    //   fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
-    //     field: value 
+      //   fbase.gameCollection.doc(window.localStorage.getItem("currentGameID")).update({
+        //     field: value 
     //   }).then(() => {
-    //     this.$store.dispatch("fetchCurrentGame")
+      //     this.$store.dispatch("fetchCurrentGame")
     //   })
     // }
   },
 }
+
 </script>
